3eefe97c908b944f9d646a39700e3fbd8c1ef0a7
ajlittlebro <nanain.mozo3@gmail.com>
2024-07-06 19:36:29 -0600

    Add Conceptos Basicos de React. Semana 2.

--- ConceptosBasicosDeReact/ConceptosBasicosDeReact/src/App.jsx
+++ ConceptosBasicosDeReact/ConceptosBasicosDeReact/src/App.jsx
@@ -1,12 +1,91 @@
 import "./App.css";
 import Heading from "./Semana1/Heading";
 import Card from "./Semana1/Card";
+import Fruits from "./Semana2/Fruits";
+import FruitsCounter from "./Semana2/FruitsCounter";
+import { useState, useRef } from "react";
+import Homepage from "./Semana3/Homepage";
+import Contact from "./Semana3/Contact";
+import AboutLittleLemon from "./Semana3/AboutLittleLemon";
+import { Routes, Route, Link } from "react-router-dom";
+import logo from "./assets/logo.png";
 
 function Header() {
   return <h1>Hello World!</h1>;
 }
 
 function App() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt("type a number");
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+
+  const [fruits] = useState([
+    { fruitName: "apple", id: 1 },
+    { fruitName: "apple", id: 2 },
+    { fruitName: "plum", id: 3 },
+  ]);
+
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  }
+
+  function toggle2() {
+    if (bird2.paused) {
+      bird2.play();
+    } else {
+      bird2.pause();
+    }
+  }
+
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  }
+
+  function minus(e) {
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  }
+
+  function times(e) {
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  }
+
+  function divide(e) {
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  }
+
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value = "";
+  }
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(0);
+  }
+
   return (
     <div>
       <Header />
@@ -19,6 +98,55 @@ function App() {
       <Card h2="First card's h2" h3="First card's h3" />
       <Card h2="Second card's h2" h3="Second card's h3" />
       <Card h2="Third card's h2" h3="Third card's h3" />
+
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Match the number between 1 and 3.</button>
+
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits} />
+      <FruitsCounter fruits={fruits} />
+
+      <nav>
+        <Link to="/" className="nav-item">
+          Homepage
+        </Link>
+        <Link to="/about" className="nav-item">
+          About Little Lemon
+        </Link>
+        <Link to="/contact" className="nav-item">
+          Contact
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<AboutLittleLemon />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+
+      <h1>Task: Add an image below</h1>
+      <img src={logo} alt="Logo" />
+
+      <button onClick={toggle1}>Caspian Tern 1</button>
+      <button onClick={toggle2}>Caspian Tern 2</button>
+
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset result</button>
+      </form>
     </div>
   );
 }
